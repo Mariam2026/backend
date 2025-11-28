4f14ad6f0f795e96052673a0c324b3264d0163b2
Emy-04 <emy.ehab73@gmail.com>
2025-11-28 16:42:53 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        dbname="EventPlanner",     # your database name
-        user="postgres",              # your PostgreSQL username
-        password="mariam",# replace with your actual password
+        dbname="EventPlanner",     # database name
+        user="postgres",              # PostgreSQL username
+        password="mariam",
         host="localhost",
         port="5432"
     )
